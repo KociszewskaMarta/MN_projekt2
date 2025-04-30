9de8ca7df543f3c6570ebddfdf1d7ce1f6adf90b
Marta Kociszewska <martakociszewska04@gmail.com>
2025-04-30 16:00:47 +0200

    added main and instruction

---
+++ main.py
@@ -0,0 +1,6 @@
+# importing libraries
+# ---------------------------------------------------
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
